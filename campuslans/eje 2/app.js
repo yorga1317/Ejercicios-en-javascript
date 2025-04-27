75f29a100f46b28ff28b449ec3e212650f05c54a
Cristian Mayorga <165953834+yorga1317@users.noreply.github.com>
2025-04-27 14:48:45 -0500

    app.js

--- campuslans/eje 2/app.js
+++ campuslans/eje 2/app.js
@@ -1,7 +1,7 @@
 console.log('este es mi ejercio #2');
 // analisis en este ejercicio se me difilcilto en la suma ya que los valores no se sumaban si no se juntaban 
-// ejemplo del erros en ves de sumar 2 + 2 que es igual a 4 me daba 22
-// arreglo busque por internet y tambien que me ayudara un IA y me indentifico el problema que era en prompt le tenia que agregar al inicio parseInt 
+// ejemplo del error en ves de sumar 2 + 2 que es igual a 4 me daba 22
+// arreglo busque por internet y tambien que me ayudara un IA y me indentifique el problema que era en prompt le tenia que agregar al inicio parseInt 
 let num1 = parseInt(prompt('ingrese el valor 1'));
 let num2 = parseInt(prompt('ingrese el valor 2'));
 // variable suma 
